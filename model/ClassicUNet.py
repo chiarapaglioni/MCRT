1273dec41b5a5d30558c0a42f4e113c6d7a3c72c
Chiara <paglionic@gmail.com>
2025-08-05 14:32:31 +0100

    added mode param to UNet

--- model/ClassicUNet.py
+++ model/ClassicUNet.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.utils.checkpoint
 
 
 class DoubleConv(nn.Module):
@@ -9,11 +8,11 @@ class DoubleConv(nn.Module):
     def __init__(self, in_channels, out_channels):
         super().__init__()
         self.double_conv = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.Conv2d(in_channels, out_channels, 3, padding=1),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
 
-            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.Conv2d(out_channels, out_channels, 3, padding=1),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
         )
@@ -23,63 +22,66 @@ class DoubleConv(nn.Module):
 
 
 class Down(nn.Module):
-    """Downscaling with maxpool then double conv"""
     def __init__(self, in_channels, out_channels):
         super().__init__()
-        self.maxpool_conv = nn.Sequential(
-            nn.MaxPool2d(kernel_size=2),
+        self.block = nn.Sequential(
+            nn.MaxPool2d(2),
             DoubleConv(in_channels, out_channels)
         )
 
     def forward(self, x):
-        return self.maxpool_conv(x)
+        return self.block(x)
 
 
 class Up(nn.Module):
-    """Upscaling then double conv"""
     def __init__(self, in_channels, out_channels, bilinear=True):
         super().__init__()
-
         if bilinear:
             self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
             self.conv = DoubleConv(in_channels, out_channels)
         else:
-            self.up = nn.ConvTranspose2d(in_channels , in_channels // 2, kernel_size=2, stride=2)
+            self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
             self.conv = DoubleConv(in_channels, out_channels)
-
-        self.bilinear = bilinear
 
     def forward(self, x1, x2):
         x1 = self.up(x1)
-
-        # Padding in case input size is not divisible by 2^n
         diffY = x2.size()[2] - x1.size()[2]
         diffX = x2.size()[3] - x1.size()[3]
-
         x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
                         diffY // 2, diffY - diffY // 2])
-
         x = torch.cat([x2, x1], dim=1)
         return self.conv(x)
 
 
 class OutConv(nn.Module):
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels):
         super().__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+        self.conv = nn.Conv2d(in_channels, 1, kernel_size=1)
 
     def forward(self, x):
         return self.conv(x)
 
 
-class UNet(nn.Module):
-    def __init__(self, n_channels, n_classes, bilinear=True):
+class UNetImportancePredictor(nn.Module):
+    def __init__(self, mode='stat', num_bins=64, bilinear=True):
+        """
+        Args:
+            mode: 'meanvar' or 'hist'
+            num_bins: number of histogram bins per channel (used if mode='hist')
+        """
         super().__init__()
-        self.n_channels = n_channels
-        self.n_classes = n_classes
+        assert mode in ['meanvar', 'hist'], "mode must be 'meanvar' or 'hist'"
+        self.mode = mode
+        self.num_bins = num_bins
         self.bilinear = bilinear
 
-        self.inc = DoubleConv(n_channels, 64)
+        # Determine number of input channels
+        if mode == 'meanvar':
+            self.in_channels = 6  # RGB mean + RGB var
+        elif mode == 'hist':
+            self.in_channels = 3 * num_bins  # RGB histograms
+
+        self.inc = DoubleConv(self.in_channels, 64)
         self.down1 = Down(64, 128)
         self.down2 = Down(128, 256)
         self.down3 = Down(256, 512)
@@ -90,18 +92,24 @@ class UNet(nn.Module):
         self.up2 = Up(512, 256 // factor, bilinear)
         self.up3 = Up(256, 128 // factor, bilinear)
         self.up4 = Up(128, 64, bilinear)
-        self.outc = OutConv(64, n_classes)
+        self.outc = OutConv(64)
 
     def forward(self, x):
-        x1 = self.inc(x)   # 64
-        x2 = self.down1(x1)  # 128
-        x3 = self.down2(x2)  # 256
-        x4 = self.down3(x3)  # 512
-        x5 = self.down4(x4)  # 1024
-
-        x = self.up1(x5, x4)  # 512
-        x = self.up2(x, x3)   # 256
-        x = self.up3(x, x2)   # 128
-        x = self.up4(x, x1)   # 64
-        logits = self.outc(x) # n_classes
-        return logits
+        """
+        Input shape:
+            [B, C, H, W] where C depends on mode
+        Output:
+            [B, 1, H, W] importance map
+        """
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        logits = self.outc(x)
+        importance = torch.sigmoid(logits)  # [0, 1] importance values
+        return importance
